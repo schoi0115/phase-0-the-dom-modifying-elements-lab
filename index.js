73b9438d8c351a3b64538103ae9eff1ca741dbe1
Shawn <schoi0115@gmail.com>
2021-06-04 02:40:13 +0000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+
+
+const newHeader = document.createElement('h1')
+document.body.appendChild(newHeader);
+newHeader.id = "victory"
+newHeader.innerHTML = '<h1>victory</h><p>Shawn is the champion</p>'
